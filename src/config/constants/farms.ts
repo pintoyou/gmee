ab2fe92d9dca74fc4e4ff75682aaf2884130dbd7
Mustafa YILDIZ <mstfyldz.bjk@icloud.com>
2021-03-14 20:10:47 +0300

    first commit

---
+++ src/config/constants/farms.ts
@@ -0,0 +1,82 @@
+import contracts from './contracts'
+import { FarmConfig, QuoteToken } from './types'
+
+const farms: FarmConfig[] = [
+  {
+    pid: 0,
+    lpSymbol: 'CAKE',
+    lpAddresses: {
+      97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
+      56: '0x7662786e7D78FC8Ed04E3517D4b9B9fEbe418cF7',
+    },
+    tokenSymbol: 'SYRUP',
+    tokenAddresses: {
+      97: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+      56: '0x7662786e7D78FC8Ed04E3517D4b9B9fEbe418cF7',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 1,
+    lpSymbol: 'GAME-BNB LP',
+    lpAddresses: {
+      97: '0xe70b7523f4bffa1f2e88d2ba709afd026030f412',
+      56: '0xf208984620aa408be350b8656afcd61fe5f3d67f',
+    },
+    tokenSymbol: 'GAME',
+    tokenAddresses: {
+      97: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+      56: '0x7662786e7D78FC8Ed04E3517D4b9B9fEbe418cF7',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 4,
+    lpSymbol: 'GAME-BUSD LP',
+    lpAddresses: {
+      97: '0xe70b7523f4bffa1f2e88d2ba709afd026030f412',
+      56: '0xb19671b42d0e4be5211fb5b0cf5ab9246c856aed',
+    },
+    tokenSymbol: 'GAME',
+    tokenAddresses: {
+      97: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+      56: '0x7662786e7D78FC8Ed04E3517D4b9B9fEbe418cF7',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 2,
+    lpSymbol: 'BUSD-BNB LP',
+    lpAddresses: {
+      97: '0x2f7682b64b88149ba3250aee32db712964de5fa9',
+      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+    },
+    tokenSymbol: 'BUSD',
+    tokenAddresses: {
+      97: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+      56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 3,
+    lpSymbol: 'ETH-BNB LP',
+    lpAddresses: {
+      97: '0xE66790075ad839978fEBa15D4d8bB2b415556a1D',
+      56: '0x70D8929d04b60Af4fb9B58713eBcf18765aDE422',
+    },
+    tokenSymbol: 'ETH',
+    tokenAddresses: {
+      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
+      56: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+]
+
+export default farms
---
+++ src/config/constants/pools.ts
@@ -0,0 +1,24 @@
+import { PoolConfig, QuoteToken, PoolCategory } from './types'
+
+const pools: PoolConfig[] = [
+  {
+    sousId: 0,
+    tokenName: 'GAME',
+    stakingTokenName: QuoteToken.GAME,
+    stakingTokenAddress: '0x7662786e7D78FC8Ed04E3517D4b9B9fEbe418cF7',
+    contractAddress: {
+      97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
+      56: '0xc3c2eAE09495D66dDc923837Bf60ee15aAe8A7f6',
+    },
+    poolCategory: PoolCategory.CORE,
+    projectLink: 'https://pancakeswap.finance/',
+    harvest: true,
+    tokenPerBlock: '10',
+    sortOrder: 1,
+    isFinished: false,
+    tokenDecimals: 18,
+  },
+  
+]
+
+export default pools
